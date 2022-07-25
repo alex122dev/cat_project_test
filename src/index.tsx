176a7d279aad6af877392ee950d7f504ad92578b
Alex <alex122dev@gmail.com>
2022-07-25 16:29:04 +0300

    marg comment

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <HashRouter> //* only for normal working in githud
+  <HashRouter> {/* only for normal working in githud */}
     <Provider store={store}>
       <App />
     </Provider>
